a4ca3982b104d6d3f1a4aba99b335beb6f722cf4
sohan immanuel <79745178+sohan70@users.noreply.github.com>
2022-03-20 23:12:26 +0530

    Uploaded LAB_8 CODES

--- 20BCAB11_Sohan.py
+++ 20BCAB11_Sohan.py
@@ -1,22 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on
+
+@author: Sohan Immanuel
+"""
+
+#Clustering 
 import pandas as pd
 import numpy as np
-import seaborn as sns
+from sklearn.cluster import AgglomerativeClustering 
 import matplotlib.pyplot as plt
-ins = pd.read_csv('insurance.csv')
-print(ins.head())
-print(ins.describe())
-sns.countplot(x = 'smoker', data= ins)
-plt.show()
-sns.scatterplot(x= 'sex',y= 'age',hue='smoker', data=ins)
-plt.show()
-sns.pairplot(ins.drop(['region'], axis = 1), hue='smoker', height=2)
-plt.show()
-sns.boxenplot('charges', data = ins)
-plt.show()
-sns.heatmap(ins.corr(), data = ins)
-plt.show()
-x = ins.corr(method = 'pearson')
-print(x) 
-sns.heatmap(ins.corr(method='pearson'))
-plt.show()
+from scipy.cluster.hierarchy import dendrogram,linkage
 
+df = pd.read_csv("shopping_data.csv")
+df.head()
+df.info()
+genre = pd.get_dummies(df['Genre'])
+df.drop(['Genre'],axis=1,inplace=True)
+df = pd.concat([df,genre],axis=1)
+df.info()
+df=np.array(df)
+
+Z = linkage(df, method = "ward") 
+dendro = dendrogram(Z) 
+plt.title('Dendogram') 
+plt.ylabel('Euclidean distance') 
+plt. show()
+
+ac=AgglomerativeClustering(n_clusters=2,affinity="euclidean",linkage="ward") 
+labels=ac.fit_predict(df) 
+plt.figure(figsize = (8,5)) 
+plt.scatter(df[labels == 0,0], df[labels == 0,1], c='red')
+plt.scatter(df[labels == 1,0], df[labels == 1,1], c='blue') 
+plt.scatter(df[labels == 2,0], df[labels == 2,1], c='green') 
+plt.scatter(df[labels == 3,0], df[labels == 3,1], c='black') 
+plt.scatter(df[labels == 4,0], df[labels == 4,1], c='orange') 
+plt.show()
